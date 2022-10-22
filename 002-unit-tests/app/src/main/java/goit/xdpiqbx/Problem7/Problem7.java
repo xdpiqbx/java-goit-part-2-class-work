0d38abb0bc0566e7d6ab583733d21e4fefdd7710
xdpiqbx <xdpiqbx@gmail.com>
2022-10-22 13:58:48 +0300

    in process...

--- 002-unit-tests/app/src/main/java/goit/xdpiqbx/Problem7/Problem7.java
+++ 002-unit-tests/app/src/main/java/goit/xdpiqbx/Problem7/Problem7.java
@@ -2,6 +2,13 @@ package goit.xdpiqbx.Problem7;
 
 public class Problem7 {
     public int romeToArabic(String rome) {
-        return 1;
+        int result = 0;
+        for (char c : rome.toCharArray()){
+            switch (rome){
+                case "I": return 1;
+                case "II": return 2;
+                default: throw new IllegalArgumentException("Not implemented");
+            }
+        }
     }
 }
--- 002-unit-tests/app/src/test/java/goit/xdpiqbx/Problem7/Problem7Tests.java
+++ 002-unit-tests/app/src/test/java/goit/xdpiqbx/Problem7/Problem7Tests.java
@@ -3,17 +3,25 @@ package goit.xdpiqbx.Problem7;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 class Problem7Tests {
     @Test
-    public void testThatRomeToArabicMethodWorksOkForI(){
-        int actual = new Problem7().romeToArabic("I");
-        int expected = 1;
-        Assertions.assertEquals(expected, actual);
-    }
-    @Test
-    public void testThatRomeToArabicMethodWorksOkForII(){
-        int actual = new Problem7().romeToArabic("II");
-        int expected = 2;
-        Assertions.assertEquals(expected, actual);
+    public void testThatRomeToArabicMethodWorksOk(){
+        Map<String, Integer> testCases = new LinkedHashMap<>();
+        testCases.put("I", 1);
+        testCases.put("II", 2);
+        testCases.put("III", 3);
+        testCases.put("IV", 4);
+        testCases.put("V", 5);
+        testCases.put("VI", 6);
+        testCases.put("VII", 7);
+        testCases.put("VIII", 8);
+        testCases.put("IX", 9);
+        testCases.put("X", 10);
+        testCases.forEach((rome, expectedArabic) ->
+            Assertions.assertEquals(expectedArabic, new Problem7().romeToArabic(rome))
+        );
     }
 }
