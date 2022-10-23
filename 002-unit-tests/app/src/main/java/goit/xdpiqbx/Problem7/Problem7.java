f0f5a5e1d6d65ce9ea20f0e72ec8b37aa096c540
xdpiqbx <xdpiqbx@gmail.com>
2022-10-23 21:58:32 +0300

    arabic to rome

--- 002-unit-tests/app/src/main/java/goit/xdpiqbx/Problem7/Problem7.java
+++ 002-unit-tests/app/src/main/java/goit/xdpiqbx/Problem7/Problem7.java
@@ -9,6 +9,10 @@ public class Problem7 {
         romeArabicValues.put('I', 1);
         romeArabicValues.put('V', 5);
         romeArabicValues.put('X', 10);
+        romeArabicValues.put('L', 50);
+        romeArabicValues.put('C', 100);
+        romeArabicValues.put('D', 500);
+        romeArabicValues.put('M', 1000);
 
         int result = 0;
 
@@ -16,29 +20,33 @@ public class Problem7 {
 
         for (int i = 0; i < romeDigits.length; i++) {
             char currentDigit = romeDigits[i];
+            int currentArabicValue = romeArabicValues.get(currentDigit);
             if(i == romeDigits.length - 1){
-                result += romeArabicValues.get(currentDigit);
+                result += currentArabicValue;
             }else{
                 char nextDigit = romeDigits[i + 1];
-                int nextDigitArabicValue = romeArabicValues.get(nextDigit);
-                //....
-            }
-        }
-
-        for (char c : rome.toCharArray()){
-            switch (c){
-                case 'I':
-                    result += 1;
-                    break;
-                case 'V':
-                    result += 5;
-                    break;
-                case 'X':
-                    result += 10;
-                    break;
-                default: throw new IllegalArgumentException("Not implemented");
+                int nextArabicValue = romeArabicValues.get(nextDigit);
+                if(nextArabicValue > currentArabicValue){
+                    result += nextArabicValue - currentArabicValue;
+                }else{
+                    result += nextArabicValue + currentArabicValue;
+                }
+                i++;
             }
         }
         return result;
+    }
+
+    public String arabicToRome(int arabic) {
+        Map<Character, Integer> romeArabicValues = new HashMap<>();
+        romeArabicValues.put('I', 1);
+        romeArabicValues.put('V', 5);
+        romeArabicValues.put('X', 10);
+        romeArabicValues.put('L', 50);
+        romeArabicValues.put('C', 100);
+        romeArabicValues.put('D', 500);
+        romeArabicValues.put('M', 1000);
+
+        return "I";
     }
 }
--- 002-unit-tests/app/src/test/java/goit/xdpiqbx/Problem7/Problem7Tests.java
+++ 002-unit-tests/app/src/test/java/goit/xdpiqbx/Problem7/Problem7Tests.java
@@ -3,6 +3,7 @@ package goit.xdpiqbx.Problem7;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -20,11 +21,43 @@ class Problem7Tests {
         testCases.put("VIII", 8);
         testCases.put("IX", 9);
         testCases.put("X", 10);
+
+        testCases.put("C", 100);
+        testCases.put("M", 1000);
+        testCases.put("L", 50);
+        testCases.put("D", 500);
+
+        testCases.put("LIII", 53);
+        testCases.put("XCVIII", 98);
+        testCases.put("DLX", 560);
+
         testCases.forEach((rome, expectedArabic) ->
             Assertions.assertEquals(
                     expectedArabic,
                     new Problem7().romeToArabic(rome),
                     rome)
+        );
+    }
+    @Test
+    public void testThatArabicToRomeMethodWorksOk(){
+        Map<Integer, String> testCases = new HashMap<>();
+        testCases.put(1, "I");
+        testCases.put(2, "II");
+        testCases.put(3, "III");
+        testCases.put(4, "IV");
+        testCases.put(5, "V");
+        testCases.put(6, "VI");
+        testCases.put(7, "VII");
+        testCases.put(8, "VIII");
+        testCases.put(9, "IX");
+        testCases.put(10, "X");
+
+        testCases.forEach((arabic, expectedRome) ->
+            Assertions.assertEquals(
+                    expectedRome,
+                    new Problem7().arabicToRome(arabic),
+                    Integer.toString(arabic)
+            )
         );
     }
 }
